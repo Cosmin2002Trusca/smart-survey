7e47ffc102ca6957c1fd6afe1d15f67d44a614d2
Cosmin Trusca <mariancosmintrusca@gmail.com>
2024-06-15 20:35:11 +0000

    Create get_answer_data function

--- run.py
+++ run.py
@@ -17,11 +17,42 @@ def get_name_data():
     """
     Get name from survey subjects
     """
-    print("Please enter your full name")
+    print("Please enter your full name.")
     print("Example: Jack Johnson\n")
 
     data_name = input("Enter your full name here: ")
-    print(f"The name provided is {data_name}")
+    print(f"The name provided is {data_name}\n")
+
+
+def get_answer_data():
+    """
+    Get answers from survey subjects
+    """
+    print("Please answer with yes or no to the following questions.")
+    print("Example: Do you have a hidden talent?: no\n")
+
+    data_question_1 = input("Do you have a hidden talent?: ")
+    print(f"Your answer is {data_question_1}\n")
+
+    data_question_2 = input("Have you ever written a song for someone?: ")
+    print(f"Your answer is {data_question_2}\n")
+
+    data_question_3 = input("Do you believe in extraterrestrial life?: ")
+    print(f"Your answer is {data_question_3}\n")
+
+    data_question_4 = input("Do you believe in ghosts?: ")
+    print(f"Your answer is {data_question_4}\n")
+
+    data_question_5 = input("Do you believe in God?: ")
+    print(f"Your answer is {data_question_5}\n")
+
+    data_question_6 = input("Have you tried any extreme sports?: ")
+    print(f"Your answer is {data_question_6}\n")
+
+    data_question_7 = input("Have you participated in a quiz game or competition?: ")
+    print(f"Your answer is {data_question_7}\n")
+
 
 
 get_name_data()
+get_answer_data()
