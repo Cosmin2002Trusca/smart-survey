b9cea0a49b71812ff8aaeb896c23efa480bf645c
Cosmin Trusca <mariancosmintrusca@gmail.com>
2024-06-16 12:16:47 +0000

    Add is_valid_name and is_valid_answer functions and make some changes to get_survey_data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import re
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,46 +14,60 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('smart_survey')
 
 
-def get_name_data():
+
+def is_valid_name(name):
     """
-    Get name from survey subjects
+    Validates that the name is properly formatted.
+    A valid name consists of at least two words with alphabetic characters.
+    """
+    return bool(re.match(r"^[A-Za-z]+ [A-Za-z]+$", name))
+
+def is_valid_answer(answer):
+    """
+    Validates that the answer is either 'yes' or 'no'.
+    """
+    return answer.lower() in ["yes", "no"]
+
+def get_survey_data():
+    """
+    Get name and answers from survey subjects with validation.
     """
     print("Please enter your full name.")
     print("Example: Jack Johnson\n")
 
-    data_name = input("Enter your full name here: ")
-    print(f"The name provided is {data_name}\n")
+    while True:
+        data_name = input("Enter your full name here: ")
+        if is_valid_name(data_name):
+            print(f"The name provided is {data_name}\n")
+            break
+        else:
+            print("Invalid name format. Please enter your full name (first and last name).")
+
+    questions = [
+        "Do you have a hidden talent?",
+        "Have you ever written a song for someone?",
+        "Do you believe in extraterrestrial life?",
+        "Do you believe in ghosts?",
+        "Do you believe in God?",
+        "Have you tried any extreme sports?",
+        "Have you participated in a quiz game or competition?"
+    ]
+
+    answers = {}
+
+    print("Please answer with 'yes' or 'no' to the following questions.")
+    for question in questions:
+        while True:
+            answer = input(f"{question}: ")
+            if is_valid_answer(answer):
+                print(f"Your answer is {answer}\n")
+                answers[question] = answer.lower()
+                break
+            else:
+                print("Invalid answer. Please respond with 'yes' or 'no'.")
+
+    return data_name, answers
 
 
-def get_answer_data():
-    """
-    Get answers from survey subjects
-    """
-    print("Please answer with yes or no to the following questions.")
-    print("Example: Do you have a hidden talent?: no\n")
+get_survey_data()
 
-    data_question_1 = input("Do you have a hidden talent?: ")
-    print(f"Your answer is {data_question_1}\n")
-
-    data_question_2 = input("Have you ever written a song for someone?: ")
-    print(f"Your answer is {data_question_2}\n")
-
-    data_question_3 = input("Do you believe in extraterrestrial life?: ")
-    print(f"Your answer is {data_question_3}\n")
-
-    data_question_4 = input("Do you believe in ghosts?: ")
-    print(f"Your answer is {data_question_4}\n")
-
-    data_question_5 = input("Do you believe in God?: ")
-    print(f"Your answer is {data_question_5}\n")
-
-    data_question_6 = input("Have you tried any extreme sports?: ")
-    print(f"Your answer is {data_question_6}\n")
-
-    data_question_7 = input("Have you participated in a quiz game or competition?: ")
-    print(f"Your answer is {data_question_7}\n")
-
-
-
-get_name_data()
-get_answer_data()
